a6b02a3169a7d82502a5f8c82766f1120483cd64
oluwaluhmie <oeolumide@gmail.com>
2024-05-30 17:08:37 +0100

    main

--- src/Pages/Our Story/Timelines.js
+++ src/Pages/Our Story/Timelines.js
@@ -7,9 +7,42 @@ import two from "../../assets/two.svg";
 import three from "../../assets/three.svg";
 
 const Timelines = () => {
-  const [isHoveredOne, setIsHoveredOne] = useState(false);
-  const [isHoveredTwo, setIsHoveredTwo] = useState(false);
-  const [isHoveredThree, setIsHoveredThree] = useState(false);
+  const [hoveredIndexes, setHoveredIndexes] = useState([false, false, false]);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndexes((prevState) => {
+      const newState = [...prevState];
+      newState[index] = true;
+      return newState;
+    });
+  };
+
+  const timelines = [
+    {
+      year: "2016",
+      img: timelinemobileone,
+      icon: one,
+      title: "Canary Finance begins operation",
+      description:
+        "To provide our clients with exceptional financial services that meet their unique needs, while maintaining the highest standards of professionalism and integrity.",
+    },
+    {
+      year: "2018",
+      img: timelinemobiletwo,
+      icon: two,
+      title: "Canary Finance gets $1 million funding",
+      description:
+        "To provide our clients with exceptional financial services that meet their unique needs, while maintaining the highest standards of professionalism and integrity.",
+    },
+    {
+      year: "2020",
+      img: timelinemobilethree,
+      icon: three,
+      title: "Canary Finance makes $2 million in revenue",
+      description:
+        "To provide our clients with exceptional financial services that meet their unique needs, while maintaining the highest standards of professionalism and integrity.",
+    },
+  ];
 
   return (
     <div className="flex flex-col items-center bg-timelinebg bg-cover">
@@ -20,99 +53,37 @@ const Timelines = () => {
               Timelines
             </span>
             <div className="grid grid-cols-1 gap-12 md:gap-6 md:w-176 lg:w-300">
-              <div
-                className={`flex flex-col md:flex-row gap-3 md:gap-6 lg:gap-10 md:opacity-20 transition-opacity duration-300 ${
-                  isHoveredOne ? "md:opacity-100" : ""
-                }`}
-                onMouseEnter={() => setIsHoveredOne(true)}
-                onMouseLeave={() => setIsHoveredOne(true)}
-              >
-                <div className="hidden md:block pt-6 pl-2">
-                  <img src={one} alt="one" />
+              {timelines.map((timeline, index) => (
+                <div
+                  key={index}
+                  className={`flex flex-col md:flex-row gap-3 md:gap-6 lg:gap-10 transition-opacity duration-300 ${
+                    hoveredIndexes[index] ? "md:opacity-100" : "md:opacity-20"
+                  }`}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                >
+                  <div className="hidden md:block pt-6 pl-2">
+                    <img src={timeline.icon} alt={`icon-${index}`} />
+                  </div>
+                  <div className="flex items-end relative">
+                    <p className="text-5xl font-bold text-white/20 absolute p-2">
+                      {timeline.year}
+                    </p>
+                    <img
+                      src={timeline.img}
+                      alt={`timeline-${index}`}
+                      className="md:w-75 md:h-48 lg:w-90"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-1 md:w-80 lg:w-128 md:py-3 lg:py-3">
+                    <span className="font-gotham text-lg md:text-xl lg:text-2xl text-menuHover">
+                      {timeline.title}
+                    </span>
+                    <p className="text-sm md:text-base lg:text-base text-mobileMenuColor">
+                      {timeline.description}
+                    </p>
+                  </div>
                 </div>
-                <div className="flex items-end">
-                  <p className="text-5xl font-bold text-white/20 absolute p-2">
-                    2016
-                  </p>
-                  <img
-                    src={timelinemobileone}
-                    alt="timelinemobileone"
-                    className="md:w-75 md:h-48 lg:w-90"
-                  />
-                </div>
-                <div className="flex flex-col gap-1 md:w-80 lg:w-128 md:py-3 lg:py-3">
-                  <span className="font-gotham text-lg md:text-xl lg:text-2xl text-menuHover">
-                    Canary Finance begins operation
-                  </span>
-                  <p className="text-sm md:text-base lg:text-base text-mobileMenuColor">
-                    To provide our clients with exceptional financial services
-                    that meet their unique needs, while maintaining the highest
-                    standards of professionalism and integrity.
-                  </p>
-                </div>
-              </div>
-              <div
-                className={`flex flex-col md:flex-row gap-3 md:gap-6 lg:gap-10 md:opacity-20 transition-opacity duration-300 ${
-                  isHoveredTwo ? "md:opacity-100" : ""
-                }`}
-                onMouseEnter={() => setIsHoveredTwo(true)}
-                onMouseLeave={() => setIsHoveredTwo(true)}
-              >
-                <div className="hidden md:block pt-6 pl-2">
-                  <img src={two} alt="two" />
-                </div>
-                <div className="flex items-end">
-                  <p className="text-5xl font-bold text-white/20 absolute p-2">
-                    2018
-                  </p>
-                  <img
-                    src={timelinemobiletwo}
-                    alt="timelinemobiletwo"
-                    className="md:w-75 md:h-48 lg:w-90"
-                  />
-                </div>
-                <div className="flex flex-col gap-1 md:w-80 lg:w-128 md:py-3 lg:py-3">
-                  <span className="font-gotham text-lg md:text-xl lg:text-2xl text-menuHover">
-                    Canary Finance gets $1 million funding
-                  </span>
-                  <p className="text-sm md:text-base lg:text-base text-mobileMenuColor">
-                    To provide our clients with exceptional financial services
-                    that meet their unique needs, while maintaining the highest
-                    standards of professionalism and integrity.
-                  </p>
-                </div>
-              </div>
-              <div
-                className={`flex flex-col md:flex-row gap-3 md:gap-6 lg:gap-10 md:opacity-20 transition-opacity duration-300 ${
-                  isHoveredThree ? "md:opacity-100" : ""
-                }`}
-                onMouseEnter={() => setIsHoveredThree(true)}
-                onMouseLeave={() => setIsHoveredThree(true)}
-              >
-                <div className="hidden md:block pt-6 pl-2">
-                  <img src={three} alt="three" />
-                </div>
-                <div className="flex items-end">
-                  <p className="text-5xl font-bold text-white/20 absolute p-2">
-                    2020
-                  </p>
-                  <img
-                    src={timelinemobilethree}
-                    alt="timelinemobilethree"
-                    className="md:w-75 md:h-48 lg:w-90"
-                  />
-                </div>
-                <div className="flex flex-col gap-1 md:w-80 lg:w-128 md:py-3 lg:py-3">
-                  <span className="font-gotham text-lg md:text-xl lg:text-2xl text-menuHover">
-                    Canary Finance makes $2 million in revenue
-                  </span>
-                  <p className="text-sm md:text-base lg:text-base text-mobileMenuColor">
-                    To provide our clients with exceptional financial services
-                    that meet their unique needs, while maintaining the highest
-                    standards of professionalism and integrity.
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
