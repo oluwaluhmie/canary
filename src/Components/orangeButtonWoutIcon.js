6a20e1585394ba1e74600a18135e5aeaf2fea6ba
oluwaluhmie <oeolumide@gmail.com>
2024-05-31 16:46:17 +0100

    main

--- src/Components/orangeButtonWoutIcon.js
+++ src/Components/orangeButtonWoutIcon.js
@@ -3,7 +3,7 @@ import React from "react";
 const OrangeButtonWoutIcon = ({ buttonText }) => {
   return (
     <div className="w-full">
-      <button className="flex items-center justify-center text-lg border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-full gap-1.5 px-8 py-3 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end">
+      <button className="flex items-center justify-center text-lg border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-full gap-1.5 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end">
         {buttonText}
       </button>
     </div>
