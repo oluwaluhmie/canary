a7b7d0db4774522c75b377573f821df7b711c7c0
oluwaluhmie <oeolumide@gmail.com>
2024-06-03 18:16:18 +0100

    main

--- src/Pages/Culture/CultureContent.js
+++ src/Pages/Culture/CultureContent.js
@@ -26,10 +26,10 @@ import diversity from "../../assets/diversity.svg";
 const CultureContent = () => {
   return (
     <div className="flex flex-col items-center bg-white md:bg-culturebg md:bg-cover">
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col px-12 lg:px-32 pt-12 pb-16 gap-12 md:py-16 md:gap-16">
-          <div className="flex flex-col md:flex-row gap-8 lg:gap-16">
-            <div className="flex flex-col gap-1 md:gap-3 md:w-84 lg:w-148.5">
+      <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col px-5 md:px-12 pt-12 pb-16 md:py-16 lg:py-24 gap-12 md:gap-16 lg:gap-24">
+          <div className="flex flex-col md:flex-row gap-8 lg:gap-16 w-97.5 md:w-176 lg:w-300">
+            <div className="flex flex-col gap-1 md:gap-3 md:w-84 lg:w-146">
               <span className="font-gotham text-2xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-b from-blueTextGradient-start to-blueTextGradient-end">
                 Our Vision
               </span>
@@ -39,7 +39,7 @@ const CultureContent = () => {
                 of professionalism and integrity.
               </p>
             </div>
-            <div className="flex flex-col gap-1 md:gap-3 md:w-84 lg:w-148.5">
+            <div className="flex flex-col gap-1 md:gap-3 md:w-84 lg:w-146">
               <span className="font-gotham text-2xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-b from-blueTextGradient-start to-blueTextGradient-end">
                 Our Mission
               </span>
@@ -49,7 +49,7 @@ const CultureContent = () => {
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-5 pt-6 md:gap-6 md:py-6 lg:gap-12 lg:py-16">
+          <div className="flex flex-col gap-5 md:gap-6 lg:gap-12 py-6 md:py-6 lg:py-16 w-97.5 md:w-176 lg:w-300">
             <div className="flex flex-col gap-1 md:gap-3">
               <span className="font-gotham text-2xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-b from-blueTextGradient-start to-blueTextGradient-end">
                 Core Values
@@ -58,8 +58,8 @@ const CultureContent = () => {
                 AGILE
               </span>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-y-12">
-              <div className="border-b-2 border-blueBorderStroke shadow-culture md:h-78 md:w-86 lg:w-96 lg:h-84 bg-white">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-6 lg:gap-y-12">
+              <div className="border-b-2 border-blueBorderStroke shadow-culture bg-white">
                 <div className="flex items-end relative">
                   <p className="font-gotham text-white text-xl px-4 py-4 absolute">
                     Accountability
@@ -80,12 +80,12 @@ const CultureContent = () => {
                     className="hidden md:hidden lg:block"
                   />
                 </div>
-                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke md:h-34 lg:h-36">
+                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke">
                   We take responsibility for our actions and are accountable to
                   our clients, shareholders, and employees.
                 </p>
               </div>
-              <div className="border-b-2 border-growthBorder shadow-culture md:h-78 md:w-86 lg:w-96 lg:h-84 bg-white">
+              <div className="border-b-2 border-growthBorder shadow-culture bg-white">
                 <div className="flex items-end relative">
                   <p className="font-gotham text-white text-xl px-4 py-4 absolute">
                     Growth
@@ -106,12 +106,12 @@ const CultureContent = () => {
                     className="hidden md:hidden lg:block"
                   />
                 </div>
-                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke md:h-34 lg:h-36">
+                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke">
                   We are committed to continuous learning and growth, both as
                   individuals and as a company.
                 </p>
               </div>
-              <div className="border-b-2 border-menuHover shadow-culture md:h-78 md:w-86 lg:w-96 lg:h-84 bg-white">
+              <div className="border-b-2 border-menuHover shadow-culture bg-white">
                 <div className="flex items-end relative">
                   <p className="font-gotham text-white text-xl px-4 py-4 absolute">
                     Innovation
@@ -132,12 +132,12 @@ const CultureContent = () => {
                     className="hidden md:hidden lg:block"
                   />
                 </div>
-                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke md:h-34 lg:h-36">
+                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke">
                   We embrace innovation and we are always looking for new and
                   better ways to serve our clients.
                 </p>
               </div>
-              <div className="border-b-2 border-loyalBorder shadow-culture md:h-78 md:w-86 lg:w-96 lg:h-91 bg-white">
+              <div className="border-b-2 border-loyalBorder shadow-culture bg-white">
                 <div className="flex items-end relative">
                   <p className="font-gotham text-white text-xl px-4 py-4 absolute">
                     Loyalty
@@ -158,12 +158,12 @@ const CultureContent = () => {
                     className="hidden md:hidden lg:block"
                   />
                 </div>
-                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke md:h-34 lg:h-43">
+                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke">
                   We are loyal to our clients and we are committed to building
                   long-term relationships based on trust and mutual respect.
                 </p>
               </div>
-              <div className="border-b-2 border-excelBorder shadow-culture md:h-78 md:w-86 lg:w-96 lg:h-91 bg-white">
+              <div className="border-b-2 border-excelBorder shadow-culture bg-white">
                 <div className="flex items-end relative">
                   <p className="font-gotham text-white text-xl px-4 py-4 absolute">
                     Excellence
@@ -184,7 +184,7 @@ const CultureContent = () => {
                     className="hidden md:hidden lg:block"
                   />
                 </div>
-                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke md:h-34 lg:h-43">
+                <p className="text-mobileMenuColor text-base lg:text-lg px-4 lg:px-6 pt-4 lg:pt-6 pb-6 lg:pb-8 border-x-2 border-borderStroke">
                   We strive for excellence in everything we do, and we are
                   committed to delivering the highest quality services to our
                   clients.
@@ -192,15 +192,15 @@ const CultureContent = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col lg:flex-row gap-12 md:gap-6 lg:gap-24 py-6 lg:py-16 lg:w-300">
-            <div className="flex flex-col gap-6 lg:gap-12 lg:w-110">
+          <div className="flex flex-col lg:flex-row gap-12 md:gap-6 lg:gap-x-24 py-6 lg:py-16 w-97.5 md:w-176 lg:w-300">
+            <div className="flex flex-col gap-6 md:gap-12 lg:gap-12 lg:w-110">
               <span className="font-gotham text-2xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-b from-blueTextGradient-start to-blueTextGradient-end">
                 Desired Behaviours
               </span>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-5 md:gap-6 lg:gap-5 lg:w-110">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-5 md:gap-6 lg:gap-5">
                 <div>
                   <span className="text-menuHover text-base lg:text-xl">Proactive:</span>
-                  <p className="text-mobileMenuColor text-base md:w-84 lg:w-110">
+                  <p className="text-mobileMenuColor text-base">
                     Taking initiative, identifying opportunities, and seeking
                     solutions instead of waiting for direction.
                   </p>
@@ -254,20 +254,20 @@ const CultureContent = () => {
             </div>
           </div>
         </div>
-        <div className="px-10 md:px-16 lg:px-36 bg-menuTextHover md:bg-culturepoliciesbg w-full">
-          <div className="py-12 md:py-16">
-            <div className="flex flex-col gap-6 md:gap-12">
+        <div className="flex flex-col lg:items-center bg-menuTextHover md:bg-culturepoliciesbg w-full">
+          <div className="px-5 md:px-12 lg:px-0 py-12 md:py-16 lg:py-24 lg:w-300 lg:items-center">
+            <div className="flex flex-col gap-6 md:gap-12 lg:gap-16 lg:w-300">
               <span className="font-gotham text-2xl md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-b from-orangeTextGradient-start to-orangeTextGradient-end">
                 Practices and Policies
               </span> 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-4 lg:gap-5 lg:gap-x-6">
-                <div className="flex flex-col gap-3 px-4 py-5 bg-white shadow-culture lg:w-96">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-4 lg:gap-6">
+                <div className="flex flex-col gap-3 lg:gap-5 px-4 lg:px-5 py-5 lg:py-7 bg-white shadow-culture">
                   <img
                     src={performance}
                     alt="performance"
                     className="w-8 h-8 md:w-9 md:h-9 lg:w-12 lg:h-12"
                   />
-                  <div className="flex flex-col gap-1 lg:w-86">
+                  <div className="flex flex-col gap-1">
                     <span className="font-gotham text-buttonHover text-base lg:text-lg">
                       Performance-based incentives:
                     </span>
@@ -277,13 +277,13 @@ const CultureContent = () => {
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-3 px-4 py-5 bg-white shadow-culture lg:w-96">
+                <div className="flex flex-col gap-3 lg:gap-5 px-4 lg:px-5 py-5 lg:py-7 bg-white shadow-culture">
                   <img
                     src={communication}
                     alt="communication"
                     className="w-8 h-8 md:w-9 md:h-9 lg:w-12 lg:h-12"
                   />
-                  <div className="flex flex-col gap-1 lg:w-86">
+                  <div className="flex flex-col gap-1">
                     <span className="font-gotham text-buttonHover text-base lg:text-lg">
                       Open communication channels:
                     </span>
@@ -293,13 +293,13 @@ const CultureContent = () => {
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-3 px-4 py-5 bg-white shadow-culture lg:w-96">
+                <div className="flex flex-col gap-3 lg:gap-5 px-4 lg:px-5 py-5 lg:py-7 bg-white shadow-culture">
                   <img
                     src={learning}
                     alt="learning"
                     className="w-8 h-8 md:w-9 md:h-9 lg:w-12 lg:h-12"
                   />
-                  <div className="flex flex-col gap-1 lg:w-86">
+                  <div className="flex flex-col gap-1">
                     <span className="font-gotham text-buttonHover text-base lg:text-lg">
                       Learning and development opportunities:
                     </span>
@@ -309,13 +309,13 @@ const CultureContent = () => {
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-3 px-4 py-5 bg-white shadow-culture lg:w-96">
+                <div className="flex flex-col gap-3 lg:gap-5 px-4 lg:px-5 py-5 lg:py-7 bg-white shadow-culture">
                   <img
                     src={work}
                     alt="work"
                     className="w-8 h-8 md:w-9 md:h-9 lg:w-12 lg:h-12"
                   />
-                  <div className="flex flex-col gap-1 lg:w-86">
+                  <div className="flex flex-col gap-1">
                     <span className="font-gotham text-buttonHover text-base lg:text-lg">
                       Flexible work arrangements:
                     </span>
@@ -325,13 +325,13 @@ const CultureContent = () => {
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-3 px-4 py-5 bg-white shadow-culture lg:w-96">
+                <div className="flex flex-col gap-3 lg:gap-5 px-4 lg:px-5 py-5 lg:py-7 bg-white shadow-culture">
                   <img
                     src={diversity}
                     alt="diversity"
                     className="w-8 h-8 md:w-9 md:h-9 lg:w-12 lg:h-12"
                   />
-                  <div className="flex flex-col gap-1 lg:w-86">
+                  <div className="flex flex-col gap-1">
                     <span className="font-gotham text-buttonHover text-base lg:text-lg">
                       Diversity and inclusion:
                     </span>
---
+++ src/Pages/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="flex flex-col items-center bg-white md:bg-culturebg md:bg-cover">
+      <div className="flex flex-col w-full md:w-176 lg:w-200"></div>
+    </div>
+  );
+};
+
+export default Services;
