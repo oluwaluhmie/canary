c8a036315c9ff543ca6a63e27c69500e3d28b53a
oluwaluhmie <oeolumide@gmail.com>
2024-06-11 13:05:44 +0100

    main

--- src/Components/orangeButton.js
+++ src/Components/orangeButton.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const OrangeButton = ({ buttonText, imgSrc, alt }) => {
   return (
-    <div className="w-full md:w-128 lg:w-150">
+    <Link to="/account" className="w-full md:w-128 lg:w-150">
       <button className="flex items-center justify-center text-base border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-full md:w-44 lg:w-44 gap-1.5 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end">
         {buttonText}
         <img src={imgSrc} alt={alt} />
       </button>
-    </div>
+    </Link>
   );
 };
 
