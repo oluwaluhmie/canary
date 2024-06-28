0069b38e5e90e3b8bac0073850c8682cfbe4779a
oluwaluhmie <oeolumide@gmail.com>
2024-06-28 17:56:13 +0100

    main

--- src/Components/blueButton.js
+++ src/Components/blueButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const BlueButton = ({ buttonText }) => {
   return (
     <div>
-      <button className="text-base items-center justify-center lg:text-lg border-2 border-blueBorderStroke text-white bg-gradient-to-b from-blueGradient-start to-blueGradient-end w-97.5 md:w-33.5 lg:w-37.5 h-11 hover:bg-gradient-to-b hover:from-blueButton-start hover:to-blueButton-end">
+      <button className="text-base items-center justify-center lg:text-lg border-2 border-blueBorderStroke text-white bg-gradient-to-b from-blueGradient-start to-blueGradient-end w-full md:w-33.5 lg:w-37.5 h-11 hover:bg-gradient-to-b hover:from-blueButton-start hover:to-blueButton-end">
         {buttonText}
       </button>
     </div>
