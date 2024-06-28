376be9e8118232335da90148af4033dc286a0a0d
oluwaluhmie <oeolumide@gmail.com>
2024-06-28 11:15:36 +0100

    main

--- src/Components/PrivacyModal.js
+++ src/Components/PrivacyModal.js
@@ -1,18 +1,210 @@
 import React from "react";
 import Modal from "react-modal";
-import PrivacyContent from "../Pages/Privacy/PrivacyContent";
+import { Link } from "react-router-dom";
 
 const PrivacyModal = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onRequestClose={onClose}>
-      <div className="flex flex-col items-center">
-        <PrivacyContent />
-        <button
-          onClick={onClose}
-          className="flex items-center justify-center text-lg border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-32 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end"
-        >
-          Close
-        </button>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="bg-white rounded-lg overflow-hidden w-11/12 md:w-3/4 lg:w-1/2 max-h-full lg:max-h-3/4 overflow-y-auto">
+          <div className="px-5 text-justify w-full">
+            <div className="flex flex-col gap-6 md:gap-6 pt-12 pb-16">
+              <span className="text-mobileMenuColor text-base">
+                Canary Point Finance Limited is a Finance Company licensed by
+                the Central Bank of Nigeria (CBN) to provide financial services
+                through various channels, including our Website. This page
+                informs you of our policies regarding the collection, use, and
+                disclosure of personal data when you use our Services and the
+                choices you have associated with that data.
+              </span>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Information We Collect
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  We collect several types of information from you when you
+                  provide it to us for various purposes to provide and improve
+                  our services. All information supplied by you to us is
+                  collected and processed in accordance with relevant data
+                  protection laws and regulations. <br />
+                  <br />
+                  <div className="pl-1">
+                    <li>
+                      Personal Information: While using our services, we may ask
+                      you to provide us with certain personally identifiable
+                      information that can be used to contact or identify you
+                      (“Personal Data”). This may include, but is not limited
+                      to: Email address, First name and last name, Phone number,
+                      Address, State of origin, Age, Gender, Religion, etc.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Service Usage Information: When you access our services,
+                      we may collect certain information automatically,
+                      including the type of device you use, your device's unique
+                      ID, IP address, operating system, browser type, and other
+                      diagnostic data (“Usage Data”).
+                    </li>
+                    <br />
+                    <li>
+                      Cookies: Our website uses cookies to provide you with a
+                      good browsing experience and to improve our site. By
+                      visiting our website, you consent to the placement of
+                      cookies in your browser in accordance with this Privacy
+                      Policy.
+                    </li>
+                  </div>
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  How we use your information
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  We may use your personal data for purposes including, but not
+                  limited to:
+                  <br />
+                  <br />
+                  <div className="pl-1">
+                    <li>Conducting our business</li>
+                    <li>Managing our relationship with you</li>
+                    <li>Offering and providing tailored services</li>
+                    <li>Providing customer care and support</li>
+                    <li>Complying with internal policies</li>
+                    <li>Providing analysis to improve our services</li>
+                    <li>
+                      Fulfilling terms of any contract you may have with us
+                    </li>
+                    <li>Communicating with you effectively</li>
+                  </div>
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Transfer of Data
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  Your information, including Personal Data, may be transferred
+                  to and maintained on computers located outside of your
+                  jurisdiction. By using our services, you consent to the
+                  transfer of your data.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Links to Third-Party Websites
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  Our services may contain links to third-party websites. We
+                  have no control over, and assume no responsibility for, the
+                  content, privacy policies, or practices of any third-party
+                  sites or services.
+                  <br />
+                  <br />
+                  <div className="pl-1">
+                    <li>
+                      Service Providers: We may employ third-party companies and
+                      individuals to facilitate our services, perform
+                      Service-related services, or assist us in analyzing how
+                      our services are used. These third parties have access to
+                      your Personal Data only to perform these tasks on our
+                      behalf and are obligated not to disclose or use it for any
+                      other purpose.
+                    </li>
+                  </div>
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Information Sharing and Disclosure
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  We may disclose your Personal Data in the good faith belief
+                  that such action is necessary to: <br />
+                  <br />
+                  <div className="pl-1">
+                    <li>Comply with a legal obligation</li>
+                    <li>Protect and defend our rights or property</li>
+                    <li>Prevent or investigate possible wrongdoing</li>
+                    <li>Protect the personal safety of users or the public</li>
+                  </div>
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Protecting Children's Privacy
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  Our services do not address anyone under the age of 18
+                  (“Children”). We do not knowingly collect personally
+                  identifiable information from anyone under the age of 18. If
+                  you are a parent or guardian and you are aware that your
+                  Children has provided us with Personal Data, please contact
+                  us.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Personal Data Retention Period
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  We will keep your personal information for as long as it is
+                  necessary for the purposes set out in this Privacy Policy,
+                  unless a longer retention period is required or permitted by
+                  law.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Your Data Privacy Rights
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  You have the right to access, update, delete, or restrict the
+                  processing of your Personal Data. You may also request a copy
+                  of the information we hold about you, or correct any
+                  inaccuracies.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Changes to This Privacy Policy
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  We may update our Privacy Policy from time to time. We will
+                  notify you of any changes by posting the new Privacy Policy on
+                  this page. You are advised to review this Privacy Policy
+                  periodically for any changes.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-blueBorderStroke text-xl">
+                  Contact Us
+                </span>
+                <p className="text-mobileMenuColor text-base">
+                  If you have any questions or concerns about this Privacy
+                  Policy or our data practices, please contact us at{" "}
+                  <Link
+                    className="text-menuHover underline underline-offset-4"
+                    onClick={() =>
+                      (window.location = "mailto:support@canaryfinance.com")
+                    }
+                  >
+                    support@canaryfinance.com
+                  </Link>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 px-6 py-4 flex justify-end">
+            <button
+              onClick={onClose}
+              className="flex items-center justify-center text-lg border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-32 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end"
+            >
+              Close
+            </button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
--- src/Components/TermsModal.js
+++ src/Components/TermsModal.js
@@ -1,18 +1,102 @@
 import React from "react";
 import Modal from "react-modal";
-import TermsContent from "../Pages/Terms/TermsContent";
 
 const TermsModal = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onRequestClose={onClose}>
-      <div className="flex flex-col items-center">
-        <TermsContent />
-        <button
-          onClick={onClose}
-          className="flex items-center justify-center text-lg border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-32 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end"
-        >
-          Close
-        </button>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="bg-white rounded-lg overflow-hidden w-11/12 md:w-3/4 lg:w-1/2 max-h-full lg:max-h-3/4 overflow-y-auto">
+          <div className="px-5 text-justify w-full">
+            <div className="flex flex-col gap-12 md:gap-12 lg:gap-16 pt-12 pb-16 md:py-12 lg:py-24">
+              <div className="flex flex-col gap-1 md:gap-5 lg:gap-5">
+                <span className="text-blueBorderStroke text-xl md:text-2xl">
+                  General Information
+                </span>
+                <p className="text-mobileMenuColor text-sm md:text-base">
+                  1. The Finance House is under no obligation to open, create or
+                  permit the operation of the requested account until receipt of
+                  all specific document for the account. <br />
+                  <br /> 2. The Finance House shall honour all
+                  cheque/orders/instrument on your account provided such
+                  instrument are duly signed by you in line with your mandate
+                  and to debit such to your account where applicable. <br />
+                  <br />
+                  3. I/we authorize the Finance House to undertake all 'Know
+                  Your Customer' procedure as stated by government regulation,
+                  laws and Finance House policies. <br />
+                  <br /> 4. I/we agree that the Finance House may at its sole
+                  discretion and without prior notice, change the minimum
+                  balance requirement, charges or interest rate on any account
+                  i/we operate with you. <br />
+                  <br /> 5. The Finance House has the right of set-off, or to
+                  transfer balances from my various account to settle a
+                  particular account of to close my account. <br />
+                  <br />
+                  6. We shall be solely responsible for the safe keep and
+                  confidentiality of my/our debit card/PIN/User ID, password and
+                  any other valid information regarding my account with the
+                  Finance House.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1 md:gap-5 lg:gap-5">
+                <span className="text-blueBorderStroke text-xl md:text-2xl">
+                  Credit Check Consent
+                </span>
+                <p className="text-mobileMenuColor text-sm md:text-base">
+                  I understand that by opening this account, I authorise Canary
+                  Finance to conduct credit inquiries with authorized credit
+                  bureaus for the purposes of assessing my financial standing
+                  and creditworthiness. This may impact future loan applications
+                  or access to certain financial products. <br />
+                  <br />I understand that Canary Finance is committed to
+                  safeguarding my personal information and will use my credit
+                  report in compliance with applicable Nigerian data protection
+                  regulations.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1 md:gap-5 lg:gap-5">
+                <span className="text-blueBorderStroke text-xl md:text-2xl">
+                  Indemnity
+                </span>
+                <p className="text-mobileMenuColor text-sm md:text-base">
+                  The Customer hereby agrees that the Customer shall, at his/its
+                  own expense, indemnify, defend and hold harmless Canary Point
+                  Finance Ltd from and against any and all liability any other
+                  loss that may occur, arising from or relating to the operation
+                  or use of the Account or the Services or breach,
+                  non-performance or inadequate performance by the Customer of
+                  any of these Terms or the acts, errors, representations,
+                  misrepresentations, misconduct or negligence of the Customer
+                  in performance of its obligations. <br />
+                  <br /> The Customer shall keep Canary Point Finance Ltd
+                  indemnified at all times against, and save Canary Point
+                  Finance Ltd harmless from all actions, proceedings, claims,
+                  losses, damages, costs, interest (both before and after
+                  judgement) and expenses (including legal costs on a solicitor
+                  and client basis) which may be brought against or suffered or
+                  incurred by Canary Point Finance Ltd in resolving any dispute
+                  relating to the Customer's Account with Canary Point Finance
+                  Ltd or in enforcing Canary Point Finance Ltd rights under or
+                  in connection with these Terms and conditions contained
+                  herein, or which may have arisen either directly or indirectly
+                  out of or in connection with Canary Point Finance Ltd
+                  performing its obligations hereunder or accepting
+                  instructions, including but not limited to, fax and other
+                  telecommunications or electronic instructions, and acting or
+                  failing to act thereon.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 px-6 py-4 flex justify-end">
+            <button
+              onClick={onClose}
+              className="flex items-center justify-center text-lg border-2 border-menuHover text-white bg-gradient-to-b from-buttonGradient-start to-buttonGradient-end w-32 h-11 hover:bg-gradient-to-b hover:from-orangeButton-start hover:to-orangeButton-end"
+            >
+              Close
+            </button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
